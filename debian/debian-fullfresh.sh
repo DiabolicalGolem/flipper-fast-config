36867bca5f4069bbcf6c5bdcf59b99d056c7463c
DiabolicalGolem <DiabolicalGolem@github.com>
2024-09-30 17:52:16 -0600

    Added Brave-browser and VsCode modified:   debian/debian-fullfresh.sh

--- debian/debian-fullfresh.sh
+++ debian/debian-fullfresh.sh
@@ -12,8 +12,9 @@ fi
 echo -e "\033[0;32mUpdating/full-upgrading all APT packages\033[0m"
 sudo apt update | sudo apt -y full-upgrade
 
-echo -e "\033[0;32mInstalling \`git\`, \`tmux\`, and \`vim\`\033[0m"
-sudo apt install git tmux nvim
+echo -e "\033[0;32mInstalling \`git\`, \`tmux\`,  \`curl\`, \`neovim\`, and \`gpg\`\033[0m"
+sudo apt install git tmux curl
+sudo apt-get install neovim gpg
 
 echo -e "\033[0;32mInstalling 1Password\033[0m"
 echo -e "    \033[0;32mAdding 1Password repo key\033[0m"
@@ -27,5 +28,17 @@ sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 echo "    \033[0;32mInstalling 1Password\033[0m"
 sudo apt update && sudo apt install 1password
+
+echo -e "\033[0;32mInstalling Brave Browser\033[0m"
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update && sudo apt install brave-browser
+
+echo -e "\033[0;32mInstalling VsCode\033[0m"
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+
 
 rm debian-fullfresh.sh
