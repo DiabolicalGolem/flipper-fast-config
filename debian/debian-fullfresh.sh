17199c3ced79dd074824fe27d3d06ae57a2501f8
DiabolicalGolem <DiabolicalGolem@github.com>
2024-09-30 17:27:50 -0600

    modified:   debian/debian-fullfresh.sh modified:   debian/debian-fullfresh.txt

---
+++ debian/debian-fullfresh.sh
@@ -0,0 +1,27 @@
+if sudo -n true 2>/dev/null; then
+    echo "Beginning \'debian-fullfresh.sh\'"
+else
+    echo "Please execute as root!"
+    exit
+fi
+
+echo "Updating/full-upgrading all APT packages"
+sudo apt update | sudo apt full-update
+
+echo "Installing \`git\`, \`tmux\`, and \`vim\`"
+sudo apt install git tmux nvim
+
+echo "Installing 1Password"
+echo "    Adding 1Password repo key"
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo "    Adding 1Password repo"
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+echo "    Add the debsig-verify policy"
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+echo "    Installing 1Password"
+sudo apt update && sudo apt install 1password
+
+rm debian-fullfresh.sh
